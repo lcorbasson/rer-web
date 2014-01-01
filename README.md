ff49a3c916fdae994336405cbdd3feb21104275f
x0r <x0r+github@x0r.fr>
2014-01-01 21:48:23 +0100

    Be less aggressive at retrying AJAX requests
    Stop retrying AJAX requests for now in order to ease debugging.

---
+++ README.md
@@ -0,0 +1,65 @@
+# RER::Web
+
+![Screenshot](http://van-der.iiens.net/blogstuff/rer-web.png)
+
+This web app is yet another variation on the "how can I possibly display the
+timetable for the 6 next trains in any way I can imagine" theme.  Only this
+time, it tries to be classy.  It is primarily intended for display on medium
+(tablet/desktop) or large screens (TVs), while trying not to break too badly on
+cellphones.
+
+See it in action at [http://x0r.fr/rer](http://x0r.fr/rer).
+
+I know the SNCF's website does the same thing, but let's face it, this
+interface is sexier.  Also, because this was an AJAX and Javascript exercise
+for myself, please try not to be too harsh on the "but this is useless"
+remarks.
+
+# Dependencies
+
+You will need the following Perl modules:
+
+ * DateTime
+ * DateTime::Format::Strptime
+ * DBI
+ * DBD::mysql
+ * Dancer
+ * JSON::XS
+ * WWW::Mechanize
+ * Text::CSV
+ * Template::Plugin::Decode
+ * Tie::Handle::CSV
+ * YAML
+
+You will also need `git` for the `install.sh` script to work.
+
+Any kind of DBMS will do; however, you will need to load SNCF's rather massive
+GTFS data into it and therefore, I recommend MySQL, PostgreSQL or anything
+somewhat beefy.  I have tested this with MySQL without any problems.
+
+# Installing
+
+Copy `config.yml.example` to `config.yml` and edit it to suit your needs.
+
+Log into an account with administrative access on MySQL and create the database
+holding the data:
+
+	mysql> CREATE DATABASE sncf_gtfs;
+
+Create a user which only has the necessary privileges.  This is optional, but
+highly recommended (not to mention a good security practice):
+
+	mysql> CREATE USER 'rer-web' IDENTIFIED BY 'some-password';
+	mysql> GRANT SELECT ON sncf_gtfs.* TO 'rer-web'@'localhost';
+
+Finally, run `sh ./install.sh`. This install script will download a GTFS
+parsing script, download the GTFS data from SNCF's website, import it into the
+database, and import a custom-made station database as well.
+
+**Note**: SNCF update their data once a week. In order to reimport the data,
+simply run `./install.sh` again.
+
+# Deployment
+
+Deploy this as you would any Perl Dancer application.  If you really
+have no idea how to do this, read the [Dancer::Deployment Perldoc page](https://metacpan.org/module/Dancer::Deployment).
---
+++ lib/RER/Transilien.pm
@@ -0,0 +1,201 @@
+#!/usr/bin/perl
+
+package RER::Transilien;
+
+use strict;
+use warnings;
+use utf8;
+use 5.010;
+
+use JSON::XS;
+use WWW::Mechanize;
+use RER::Trains qw(uc_woac);
+use RER::Gares;
+use List::Util qw(min);
+
+
+our $url = "http://ods.ocito.com/ods/transilien/android"; 
+
+
+sub get_train_desserte {
+    my ($current_station, $train) = @_;
+
+    my $number = $train->{trainNumber};
+
+    my $mech = WWW::Mechanize->new(autocheck => 0);
+    $mech->add_header( 'User-Agent' => undef );
+
+
+    my $content = qq`[{"listOfMap":null,"map":{"trainNumber":"${number}","theoric":"false"},
+        "headers":null,"target":"\\/transilien\\/getTrainDetails","list":null,"serial":5}]`;
+
+
+    my $r = $mech->post($url, Content => $content);
+    if (!$r->is_success) {
+        die("Erreur lors de la récupération de la desserte du train ${number} : " 
+            . "sncf.mobi a renvoyé l'erreur " . $r->status_line . "\n");
+    }
+
+    my $data = decode_json $mech->content();
+    $data = $data->[0];
+
+    my @stations = map { $_->{codeGare} } @{$data->{data}};
+    my %mentions = map { $_->{codeGare} => $_->{mention} } @{$data->{data}}; 
+
+    if (grep { $_ eq $current_station } @stations) {
+        # Par défaut la desserte renvoie TOUTES les gares, y compris celles
+        # déjà desservies avant.  Enlever ceux qui ne nous intéressent pas.
+        while ($#stations > 0 && shift @stations ne $current_station) { 1 }
+    }
+
+    $train->{trainMention} = $mentions{$current_station} || 'N';
+
+    return @stations;
+}
+
+
+sub new {
+    my %param = @_;
+
+    utf8::upgrade($param{'from'});
+    utf8::upgrade($param{'to'}) if defined($param{'to'});
+
+    # Obtention des trigrammes correspondant aux gares recherchées
+    my ($trig_from, $trig_to);
+
+    my $tmp = RER::Gares::get_station_by_code($param{'from'});
+    die $param{'from'} . ": gare non trouvée" unless length($tmp) > 0;
+    $trig_from = $param{'from'};
+
+    my $mech = WWW::Mechanize->new(autocheck => 0);
+    $mech->add_header( 'User-Agent' => undef );
+
+    my $content = qq`[{"listOfMap":null,"map":{"codeDepart":"${trig_from}"},
+        "headers":null,"target":"\\/transilien\\/getNextTrains","list":null,"serial":4}]`;
+
+
+    my $r = $mech->post($url, Content => $content);
+    if (!$r->is_success) {
+        die("Erreur lors de la récupération des horaires en live : " 
+            . "sncf.mobi a renvoyé l'erreur " . $r->status_line . "\n");
+    }
+
+    my $data = decode_json $mech->content();
+    $data = $data->[0];
+    my @t = sort { $a->{trainHour} cmp $b->{trainHour} } @{$data->{'data'}};
+
+    # FIXME: cassé
+    # si gare de destination donnée, filtrer les trains qui desservent la gare
+    # @t = grep { (join ",", @{$_->{dessertes}}) =~ /\b$trig_to\b/ } @t if $trig_to;
+
+    my @messages = ();
+    @messages = @{$data->{'list'}} if defined $data->{'list'};
+    my @trains;
+
+    my $i;
+    
+    # Récupération des vrais noms de gares
+    $param{'from'} = RER::Gares::get_station_by_code($trig_from);
+    $param{'to'}   = RER::Gares::get_station_by_code($trig_to) if defined($param{'to'});
+
+    # Récupération de l'ensemble des trains
+    for (my $i = 0; $i < 6; $i++)
+    {
+        my $train = $t[$i];
+        next if not defined $train;
+
+        my $mission = $train->{trainMissionCode};
+        my $numero  = $train->{trainNumber};
+
+        my $time    = (split /\s+/, $train->{trainHour})[1];
+        my $destination = RER::Gares::get_station_by_code($train->{trainTerminus});
+        my @arr_dessertes = get_train_desserte($trig_from, $train);
+        my $platform = $train->{trainLane} || $train->{trainDock};
+
+
+        @arr_dessertes = map { RER::Gares::get_station_by_code($_) or (defined $_ ? "$_" : "<i>Gare inconnue</i>") } @arr_dessertes;
+        my $dessertes = join ' &bull; ', @arr_dessertes;
+
+        # L'attribut "trainMention" indique si le train est retardé, supprimé...
+        my $time_info;
+        my $trainclass;
+        my $col2class;
+        given ($train->{trainMention}) {
+            when ('N') { # rien = Normal
+                $time_info = $time;
+                $trainclass = 'train';
+                $col2class = 'col2';
+            }; 
+            when ([qw(I T D)]) { 
+                $time_info = 'Retardé'; 
+                $trainclass = 'train delayed';
+                $col2class = 'col2 texte';
+            };
+            when ('S') { 
+                $time_info = 'Supprimé';
+                $trainclass = 'train canceled';
+                $col2class = 'col2 texte';
+            };
+            when ('P') { 
+                $time_info = 'À l\'approche';
+                $trainclass = 'train';
+                $col2class = 'col2 approche';
+            };
+            when ('Q') { 
+                $time_info = 'À quai';
+                $trainclass = 'train';
+                $col2class = 'col2 texte';
+            };
+            default { 
+                $time_info = "$time (MENTION '" . ($train->{trainMention} || 'undef') . "' INCONNUE)"; 
+                $trainclass = 'train';
+                $col2class = 'col2 texte';
+            };
+        }
+
+        # Un peu de nettoyage (et de passage en UTF-8) pour les infos concernant
+        # chaque train
+        eval {
+            map
+            {
+                $_ = '' unless defined($_);
+                utf8::decode($_);
+            } ($mission, $time, $destination, $dessertes, $platform);
+        };
+
+        # Obtention du retard
+        my $delay = RER::Gares::get_delay($numero, $trig_from, $time);
+        $delay = RER::Gares::format_delay($delay);
+
+        # "massage" du numéro de trains dans le cas d'une gare RATP
+
+        $numero =~ s/RATP-([^-]+)-.*$/$1/;
+        push @trains, { 
+            mission => $mission, 
+            numero  => $numero,
+            time    => $time_info,
+            destination => $destination,
+            dessertes   => $dessertes,
+            platform => $platform,
+            col2class => $col2class,
+            trainclass => $trainclass,
+            retard => $delay,
+            ligne => RER::Gares::get_ligne($numero, $train->{trainTerminus}),
+        };
+    }
+
+    my $obj = RER::Trains::new(
+        $param{'from'},
+        $param{'to'},
+        \@trains,
+        \@messages);
+
+    return $obj;
+}
+
+
+
+
+1;
+
+# vi:ts=4:sw=4:et
